371b295022755e5c0b9cb27ddfa4631773b40aa9
Deepwater-Exploration <alexvazquez@exploredeepwater.com>
2021-12-17 06:16:08 +0000

    Switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'furo'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Disable showing the sourcelink option for each page
 html_show_sourcelink = False
